eec17e016c6efeab829c19cc253e2b61b276baef
Saeed Ali <saeedaliofficial77@gmail.com>
2024-12-22 00:27:57 +0500

    initial commit

---
+++ app.py
@@ -0,0 +1,20 @@
+# print("HELLO John")
+# print("HELLO jon")
+# print("HELLO WOLRD")
+# print("HELLO WOLRD")
+# print("HELLO WOLRD")
+# print("HELLO WOLRD")
+# print("HELLO WOLRD")
+
+# myName: str  = "Hello SAEED"
+# print(myName)
+# print(myName)
+# print(myName)
+# print(myName)
+# print(myName)
+
+# #OPRETATORS
+
+
+
+
